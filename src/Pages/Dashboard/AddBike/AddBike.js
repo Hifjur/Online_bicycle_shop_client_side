6166f8b61b5c809d7ce49b162ec2f04fe1fbce04
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-11-11 23:40:06 +0600

    add bike

--- src/Pages/Dashboard/AddBike/AddBike.js
+++ src/Pages/Dashboard/AddBike/AddBike.js
@@ -1,10 +1,100 @@
-import React from 'react';
+import { Alert, Button, Container, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const AddBike = () => {
+    
+    const [productInfo, setProdutInfo] = useState({})
+    const [bikeAdded, setBikeAdded] = useState(false);
+
+    const handleOnBlur = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newInfo = { ...productInfo };
+        newInfo[field] = value;
+        setProdutInfo(newInfo);
+    }
+
+    const handleOrderConfirmation = e => {
+        
+        e.preventDefault();
+        
+        fetch('http://localhost:5000/bikes', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(productInfo)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    setBikeAdded(true);
+                    
+
+                }
+            })
+
+        e.preventDefault();
+    }
     return (
-        <div>
-            <h2>nikes adadom</h2>
-        </div>
+        <Container>
+            
+            
+            <Typography variant="h1" sx={{fontWeight:500, padding: '30px', color: 'white', marginTop: 3, backgroundColor:'rgb(35, 34, 34)'}}>ADD NEW BIKES</Typography>
+            
+            {bikeAdded && <Alert severity="success">Purchased Successfully</Alert>}
+
+            <form style={{backgroundColor: 'white', paddingTop: '20px', height: '500px'}} onSubmit={handleOrderConfirmation}>
+
+                <TextField
+                    sx={{ width: '70%', m: 1 }}
+                    name="name"
+                    onBlur={handleOnBlur}
+                    label="Product Name"
+                    id="outlined-size-small"
+                    size="small"
+                />
+
+                <TextField
+                    sx={{ width: '70%', m: 1 }}
+                    name="description"
+                    onBlur={handleOnBlur}
+                    label="Description"
+                    id="outlined-size-small"
+                    size="small"
+                />
+                <TextField
+                    sx={{ width: '70%', m: 1 }}
+                    name="price"
+                    onBlur={handleOnBlur}
+                    label="Price"
+                    id="outlined-size-small"
+                    size="small"
+                />
+                <TextField
+                    sx={{ width: '70%', m: 1 }}
+                    name="img"
+                    onBlur={handleOnBlur}
+                    label="Image"
+                    id="outlined-size-small"
+                    size="small"
+                />
+                <TextField
+                    sx={{ width: '70%', m: 1 }}
+                    name="catagory"
+                    onBlur={handleOnBlur}
+                    label="Catagory"
+                    id="outlined-size-small"
+                    size="small"
+                />
+                <br />
+                <Button sx={{ backgroundColor: '#C54B47', m: 1 }} type="submit" variant="contained">Add To Database</Button>
+            </form>
+            <NavLink style={{textDecoration:'none'}} to='/dashboard'>
+            <Button sx={{margin: '20px'}} variant="outlined">Dashboard</Button>
+            </NavLink>
+        </Container>
     );
 };
 
