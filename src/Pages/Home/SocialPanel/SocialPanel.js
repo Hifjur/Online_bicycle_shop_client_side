c6ac391dd4060887aa502c9c3a59e02f494df9ad
HIFJUR RAHMAN <hifjur1313@gmail.com>
2021-11-13 16:32:46 +0600

    social panel

--- src/Pages/Home/SocialPanel/SocialPanel.js
+++ src/Pages/Home/SocialPanel/SocialPanel.js
@@ -9,6 +9,7 @@ import { Button, Grid, Typography } from '@mui/material';
 import SocialPanlePhoto from '../SocialPanelPhoto/SocialPanlePhoto';
 import { NavLink } from 'react-router-dom';
 import { Share } from '@mui/icons-material';
+import { Box } from '@mui/system';
 
 
 const SocialPanel = () => {
@@ -28,7 +29,7 @@ const SocialPanel = () => {
             alignItems="center"
             justifyContent="center"
 
-        >   <div style={{maxWidth:'80%'}}>
+        >   <Box sx={{maxWidth:{xs:'90%', md:'80%'}}}>
                 <Typography variant="h2" sx={{ fontSize:{xs:40, md:90}, fontWeight: 'bold', color: 'white', paddingY: 10, backgroundColor: '#C54B47' }}>Custormer's Corner</Typography>
                 <ImageList sx={{ justifyContent: 'center', height: 713, margin: 0, backgroundColor: 'rgb(35, 34, 34)', padding: 4, border: ' 5px solid #C54B47' }}>
                     <ImageListItem key="Subheader" cols={2}>
@@ -41,7 +42,7 @@ const SocialPanel = () => {
                 <NavLink style={{ textDecoration: 'none' }} to='/dashboard/social'>
                     <Button sx={{ backgroundColor: '#C54B47', m: 1 }} type="submit" variant="contained"> <Share sx={{marginX:1}}/> Share Your Photo</Button>
                 </NavLink>
-            </div>
+            </Box>
         </Grid>
     );
 }
