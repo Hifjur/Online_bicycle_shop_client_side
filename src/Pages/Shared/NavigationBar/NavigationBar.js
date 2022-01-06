61579473e951342712dc8fe5c185046de5ad8a22
HIFJUR RAHMAN <hifjur1313@gmail.com>
2022-01-06 20:49:29 +0600

    navitaoin button for phone with menu added

--- src/Pages/Shared/NavigationBar/NavigationBar.js
+++ src/Pages/Shared/NavigationBar/NavigationBar.js
@@ -9,12 +9,28 @@ import { Home, Login, Logout, Person } from '@mui/icons-material';
 import useAuth from '../../../Hooks/useAuth';
 import { NavLink } from 'react-router-dom';
 import NavBarPhone from '../NavbarPhone/NavbarPhone'
+import { Menu, MenuItem } from '@mui/material';
+import menueIcon from "../../../images/menu_white_24dp.svg";
 
-
+const navStylePhone = {
+    padding: 20,
+    fontSize: "20px",
+    fontWight: "400px",
+    textDecoration: "none",
+    color: "black",
+    fontFamily: "Chakra Petch",
+  };
 
 const Navigation = () => {
     const { user, logout } = useAuth();
-
+    const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -30,7 +46,69 @@ const Navigation = () => {
 
                     </IconButton>
                     <Box style={{ display: { xs: 'block', md: 'hidden' } }}>
-                        <NavBarPhone></NavBarPhone>
+                    <Button
+        sx={{ display: { md: "none", xs: "block" }, margin: 0 }}
+        id="basic-button"
+        aria-controls="basic-menu"
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <img style={{ fill: "white" }} src={menueIcon} alt="" />
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+        sx={{ width: "100%", margin: 0 }}
+      >
+        <MenuItem onClick={handleClose}>
+          <NavLink style={navStylePhone} to="/">
+            Home
+          </NavLink>
+        </MenuItem>
+        {user?.email ? (
+          <Box>
+            <MenuItem onClick={handleClose}>
+            <NavLink style={navStylePhone} to="/dashboard">
+            Dashboard
+          </NavLink>
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <Button
+                sx={{
+                  m: 2,
+                  display: { xs: "block", md: "hidden" },
+                  backgroundColor: "#1D2440",
+                  color: "#D3BDBD",
+                }}
+                variant="contained"
+                onClick={logout}
+                color="inherit"
+              >
+                <Logout></Logout>
+              </Button>
+            </MenuItem>
+          </Box>
+        ) : (
+          <Box>
+            <MenuItem onClick={handleClose}>
+              <NavLink
+                style={{ m: 2, textDecoration: "none", color: "white" }}
+                to="/login"
+              >
+                <Button color="inherit">
+                  <Login></Login>
+                </Button>
+              </NavLink>
+            </MenuItem>
+          </Box>
+        )}
+      </Menu>
                     </Box>
                     <NavLink style={{ alignContent: 'center', m: 2, display: { xs: 'inline', md: 'inline' }, textDecoration: 'none', color: 'white', }} to='/'>
                         <Button sx={{ m: 2, display: { xs: 'none', md: 'inline' }, backgroundColor: 'rgb(35, 34, 34)', color: '#D3BDBD' }} variant="contained" color="inherit"><Home/>
